da262880ed6271c824a80c31da5e12f93fc28d6f
Izunna <izualiozor@gmail.com>
2021-05-29 20:47:28 +0100

    web test

--- templates/ADMIN/login/home.php
+++ templates/ADMIN/login/home.php
@@ -48,7 +48,7 @@
 				</div>
 
 				<div class="container-login100-form-btn">
-					<button class="login100-form-btn">za
+					<button class="login100-form-btn">
 						Login
 					</button>
 				</div>
