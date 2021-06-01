8b87e66c745753d21c6b6cad85f3f45407fa3366
Izunna <izualiozor@gmail.com>
2021-06-01 18:37:54 +0100

    make it better

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '21.01.14',
+    'version' => '21.01.14.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => 'golojan/anthill',
+  ),
+  'versions' => 
+  array (
+    'golojan/anthill' => 
+    array (
+      'pretty_version' => '21.01.14',
+      'version' => '21.01.14.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+  ),
+);
